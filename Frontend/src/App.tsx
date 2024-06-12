ea24c38669259249da0d3be1fc4937700b1aed92
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-06-12 21:45:22 +0530

    Footer created

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import AuthContextProvider from "./context/Auth/AuthContextProvider";
 
@@ -7,6 +8,10 @@ function App() {
       <nav className='fixed w-screen flex justify-center'>
         <Header />
       </nav>
+      <div className='flex flex-col h-screen'>
+        <div className='flex-grow'>{/* Your main content here */}</div>
+        <Footer />
+      </div>
     </AuthContextProvider>
   );
 }
