7c1c966f664dd9f8ba5e4951a8b035d11e590342
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-06-14 02:12:38 +0530

    Header modified

--- Frontend/src/components/Header/Header.tsx
+++ Frontend/src/components/Header/Header.tsx
@@ -13,22 +13,22 @@ function Header() {
   };
 
   return (
-    <div className='navbar backdrop-blur-3xl h-min font-sans sticky m-0 p-0 mt-2  rounded w-full md:w-[70%]'>
+    <div className='navbar  h-min font-sans sticky p-0 lg:mt-2 mt-0 rounded flex justify-between w-full '>
       <div className='flex-1 lg:gap-6 gap-0 lg:p-4 p-0'>
-        <div className='btn btn-ghost rounded '>
+        <div className='btn btn-ghost rounded'>
           <Svg />
         </div>
 
         <input
           type='text'
-          className='input w-full bg-base-200 rounded lg:mr-4 '
+          className='input w-full bg-base-200 rounded lg:mr-4'
           placeholder='Search for product'
         />
       </div>
       <div className='hidden md:flex gap-4 mx-3 text-xl'>
         <NavElements />
       </div>
-      <div className='flex md:hidden '>
+      <div className='flex md:hidden'>
         <Hamburger
           toggled={isMenuOpen}
           toggle={toggleMenu}
@@ -37,8 +37,8 @@ function Header() {
           color='#291334'
         />
         {isMenuOpen && (
-          <div className='transition-all duration-700 absolute top-16 left-0 right-0 backdrop-blur-3xl  shadow-xl rounded'>
-            <div className='flex flex-col items-center py-2'>
+          <div className='transition-all duration-700 absolute top-16 left-0 right-0  shadow-xl rounded bg-white/90 rounded-b-3xl'>
+            <div className='flex flex-col items-center py-2  w-full'>
               <NavElements />
             </div>
           </div>
