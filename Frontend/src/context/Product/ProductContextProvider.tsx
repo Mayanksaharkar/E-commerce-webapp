af65f2fc678ab0ad9cf0a4ce202845f6b0f040c4
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-06-16 15:07:26 +0530

    add: categories page

--- Frontend/src/context/Product/ProductContextProvider.tsx
+++ Frontend/src/context/Product/ProductContextProvider.tsx
@@ -1,44 +1,50 @@
+import { ReactNode } from "react";
+
 import { useState, useEffect } from "react";
 import { Products } from "../../Models/Product";
 import ProductContext from "./ProductContext";
-function ProductContextProvider({ children }) {
+
+interface ComponentProps {
+  children: ReactNode;
+  // add other props here if needed
+}
+
+function ProductContextProvider({ children }: ComponentProps) {
   const [products, setProducts] = useState<Products>();
   const [categories, setCategories] = useState([]);
 
   const base_url = "http://localhost:3000/product/";
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(base_url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      await setProducts(data);
+      console.log(products);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+  const getCategories = async () => {
+    try {
+      const response = await fetch(`${base_url}categories`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
 
+      await setCategories(data);
+    } catch (error) {
+      console.error("Error getting Categories:", error);
+    }
+  };
   useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch(base_url, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
-        setProducts(data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-
-    const getCategories = async () => {
-      try {
-        const response = await fetch(`${base_url}categories`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        const data = await response.json();
-        console.log(data);
-        setCategories(data);
-      } catch (error) {
-        console.error("Error getting Categories:", error);
-      }
-    };
-
     fetchProducts();
     getCategories();
   }, []);
@@ -56,6 +62,7 @@ function ProductContextProvider({ children }) {
         setProducts,
         categories,
         setCategories,
+        fetchProducts,
       }}
     >
       {children}
