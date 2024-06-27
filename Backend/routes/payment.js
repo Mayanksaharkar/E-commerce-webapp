e3df637d56b80cd97179fd87765842a0bcdb4efd
Mayank Saharkar <mayanksaharkar@gmail.com>
2024-06-28 01:17:43 +0530

    typo

--- Backend/routes/payment.js
+++ Backend/routes/payment.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/auth");
 
 const { checkout } = require("../controllers/peyment_controller");
 
